b0178664cda77c3ad66f035d387c4313ed4fd608
jmarino <jim.marino@gmail.com>
2015-07-24 16:52:16 +0200

    add another queue

--- src/main/java/io/massiv/samples/sales/order/SupplierService.java
+++ src/main/java/io/massiv/samples/sales/order/SupplierService.java
@@ -1,6 +1,13 @@
 package io.massiv.samples.sales.order;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,8 +16,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class SupplierService {
 
+    @Autowired
+    protected JmsTemplate jmsTemplate;
+
     @JmsListener(destination = "supplier.queue")
-    public void receiveMessage(String message) {
+    public void receiveMessage(final String message) {
         System.out.println("Received <" + message + ">");
+        jmsTemplate.send("backorder.queue", new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage(message);
+            }
+        });
     }
 }
